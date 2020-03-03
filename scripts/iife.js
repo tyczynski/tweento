8fe0b587b959ced1c50d5363b00d15b8402cd77a
Przemysław Tyczyński <przemyslaw@tyczynski.pl>
2020-03-03 16:18:25 +0100

    chore(deps): update packages

--- scripts/iife.js
+++ scripts/iife.js
@@ -1,15 +1,15 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 import output from './partials/output';
 import minify from './partials/minify';
 import input from './partials/input';
 
 export default {
-	input,
-	output: Object.assign({}, output, {
-		file: 'lib/tweento.iife.js',
-		format: 'iife',
-	}),
-	plugins: minify.concat([resolve(), commonjs()]),
+  input,
+  output: Object.assign({}, output, {
+    file: 'lib/tweento.iife.js',
+    format: 'iife',
+  }),
+  plugins: minify.concat([resolve(), commonjs()]),
 };
